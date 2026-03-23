dc38aae04f060065eb20abd439f043bc30ccb41d
Tiago Turra <Tiago.Turra@mastercard.com>
2026-03-23 08:34:46 -0300

    updating manager to use helper to get the context

--- src/components/DYManager/DYManager.jsx
+++ src/components/DYManager/DYManager.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
-import { useLocation, useParams, matchPath } from 'react-router-dom';
+import { useLocation, matchPath } from 'react-router-dom';
 import { useCart } from '../../context/CartContext';
+import { Helper } from '../../helpers/helper';
 
 const DY_SECTION_ID = '8787656';
 
@@ -10,29 +11,7 @@ export const DYManager = () => {
 
   useEffect(() => {
     // 1. Determine Context
-    let context = { type: 'OTHER' };
-
-    switch(true) {
-      case location.pathname === '/':
-        context = { type: 'HOMEPAGE' }
-        break
-      case location.pathname.includes('/category/'):
-        const categoryMatch = matchPath('/category/:categoryName', location.pathname)
-        const search = new URLSearchParams(location.search)
-        const data = [categoryMatch.params.categoryName]
-        search.get('sub') ? data.push(search.get('sub')) : ''
-        search.get('item') ? data.push(search.get('item')) : ''
-        context = { type: 'CATEGORY', data }
-        break
-      case location.pathname.includes('/product/'):
-        const productMatch = matchPath('/product/:productId', location.pathname)
-        context = { type: 'PRODUCT', data: [productMatch.params.productId] }
-        break
-      case location.pathname.includes('/cart'):
-        const itemIds = cart.map(item => String(item.id))
-        context = { type: 'CART', data: itemIds }
-        break
-    }
+    const context = Helper.getDYContext(cart);
 
     // 2. Update DY Global Object
     window.DY = window.DY || {};
